1f4f9c89de7bbfb31523bd1c2eb35051a883e0cc
surya <venkata.surya@onpassive.com>
2021-02-22 11:52:45 +0530

    added streams

---
+++ src/com/streams/Stream1.java
@@ -0,0 +1,34 @@
+package com.streams;
+
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Stream1 {
+
+	public static void main(String[] args) {
+		IntStream.range(1, 11).forEach(System.out::print);
+		System.out.println();
+		IntStream.range(1, 11).skip(5).forEach(System.out::print);
+		System.out.println();
+		System.out.println(IntStream.range(1, 11).sum());
+		System.out.println();
+		
+		Stream.of("AL","AVl","Albert").findFirst().ifPresent(System.out::print);
+		System.out.println();
+		
+		String[]  str= {"ABc","ASB","SBI","WAS","ORT"};
+		Arrays.stream(str).filter(x->x.startsWith("S")).sorted().forEach(System.out::print);
+		System.out.println();
+		
+		Arrays.stream(new int[] {2,4,5,6,7,8}).map(x->x*x).average().ifPresent(System.out::print);
+		System.out.println();
+		
+		//List<String> peple=Arrays.asList("ABc","ASB","SBI","WAS","ORT");
+		//peple.stream().map(String::toLowerCase).
+		
+		IntSummaryStatistics summry=IntStream.of(2,4,5,6,7,8).summaryStatistics();
+		System.out.println(summry);
+	}
+}
